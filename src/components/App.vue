b82703688d38a9be23f268863da4f8d3f050410e
Vladimir Avgustov <vavgustov@gmail.com>
2018-04-17 23:54:37 +0300

    New version.

---
+++ src/components/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app">
+    <div id="particles-js"></div>
+    <section class="hero is-fullheight has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-spaced is-size-2 animated fadeInLeft" v-show="showTitle">
+            Vladimir Avgustov
+          </h1>
+          <h2 class="subtitle is-size-4 animated fadeInRight" v-show="showDescription">
+            Ruby on Rails | Full Stack Web Developer
+          </h2>
+          <div class="social is-size-2 animated fadeInLeft" v-show="showSocial">
+            <a id="github" href="https://github.com/vavgustov" target="_blank">
+              <i class="fa fa-github"></i>
+            </a>
+            <a id="linkedin" href="https://www.linkedin.com/in/vavgustov" target="_blank">
+              <i class="fa fa-linkedin"></i>
+            </a>
+            <a id="email" href="mailto:vavgustov@yandex.ru">
+              <i class="fa fa-envelope"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import 'particles.js'
+
+  export default {
+    name: 'app',
+
+    data() {
+      return {
+        showTitle: false,
+        showDescription: false,
+        showSocial: false
+      }
+    },
+
+    mounted() {
+      particlesJS.load('particles-js', 'static/particles.json')
+
+      setTimeout(() => {
+        this.showTitle = true
+      }, 500)
+
+      setTimeout(() => {
+        this.showDescription = true
+      }, 1000)
+
+      setTimeout(() => {
+        this.showSocial = true
+      }, 1500)
+    }
+  }
+</script>
+
+<style lang="scss">
+  $fa-font-path : '~font-awesome/fonts';
+  $bg-color: #282c37;
+
+  @import "~bulma";
+  @import "~animate.css/animate";
+  @import "~font-awesome/scss/font-awesome";
+
+  html, body {
+    background: $bg-color;
+  }
+
+  .hero {
+    a {
+      color: $white;
+      margin: 0.5rem;
+
+      &:hover {
+        color: $turquoise;
+      }
+    }
+
+    .container {
+      min-height: 220px;
+    }
+  }
+
+  .title,
+  .subtitle {
+    color: $white;
+  }
+
+  .animated {
+    animation-duration: 0.4s;
+  }
+
+  #particles-js {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './components/App'
 
 Vue.config.productionTip = false
 
@@ -10,4 +10,16 @@ new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
+})
+
+let tracking = () => {
+  const link_types = ['github', 'linkedin', 'email']
+  mixpanel.track('page view')
+  link_types.forEach((link_type) => {
+    mixpanel.track_links('#' + link_type, 'click link: ' + link_type)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  tracking()
 })
